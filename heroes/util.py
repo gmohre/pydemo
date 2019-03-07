b9b254dcd1233955825727b311326430512f7c48
Glenn Mohre <glenn.mohre@gmail.com>
2019-03-07 18:48:37 -0500

    moved hash func to util

---
+++ heroes/util.py
@@ -0,0 +1,11 @@
+from flask import current_app as app
+
+def marvel_hash():
+    ts = str(int(time.time()))
+    m = hashlib.md5()
+    key = ''.join((
+        ts,
+        app.config['PRIVATE_API_KEY'],
+        app.config['PUBLIC_API_KEY']))
+    m.update(key.encode('utf-8'))
+    return ts, m.hexdigest(), app.config['PUBLIC_API_KEY']
