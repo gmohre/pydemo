09e248eed97be80e34e27f21a4032bdab1442f40
Glenn Mohre <glenn.mohre@gmail.com>
2019-03-08 17:33:04 -0500

    added VUE client

---
+++ client/src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="container">
+    <h3>{{ name }}</h3>
+    <img class="comic" :src="img">
+    <button type="button" class="btn btn-primary">{{ name }}</button>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Hero',
+  props: ['id', 'marvel_id', 'img', 'name']
+/*   data () {
+    return {
+      id: 21,
+      marvel_id: 1009243,
+      name: 'Colossus',
+      thumbnail_url:
+        'http://i.annihil.us/u/prod/marvel/i/mg/6/e0/51127cf4b996f.jpg'
+    }
+  } */
+}
+</script>
---
+++ client/src/components/HeroList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="comicslist">
+    <ul>
+      <li is="Hero" v-bind:key="index" v-bind:img="hero.thumbnail_url" v-bind:name="hero.name" v-for="(hero, index) in heroes"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Hero from './Hero.vue'
+let API_URL = 'http://localhost:5000/heroes'
+export default {
+  name: 'HeroList',
+  components: {
+    Hero: Hero
+  },
+  data () {
+    return {
+      heroes: [],
+      index: 0,
+      bottom: false
+    }
+  },
+  methods: {
+    bottomVisible () {
+      const scrollY = window.scrollY
+      const visible = document.documentElement.clientHeight
+      const pageHeight = document.documentElement.scrollHeight
+      const bottomOfPage = visible + scrollY >= pageHeight
+      return bottomOfPage || pageHeight < visible
+    },
+    getInitialHeroes () {
+      axios.get(API_URL).then(response => {
+        if (!response.error) {
+          let heroes = response.data
+          this.index = heroes[heroes.length - 1]['num'] - 1
+          this.heroes.push(...heroes)
+        }
+      })
+    }
+  },
+  watch: {
+    bottom (bottom) {
+      if (bottom) {
+        console.log('bottom')
+        // this.addComic();
+      }
+    }
+  },
+  beforeMount () {
+    this.getInitialHeroes()
+  },
+  created () {
+    // window.addEventListener('scroll', () => {
+    //  this.bottom = this.bottomVisible()
+    // })
+    // this.addComic()
+    this.getInitialHeroes()
+  }
+}
+</script>
