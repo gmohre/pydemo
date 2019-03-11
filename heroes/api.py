854d94344544b5f9de00764bf0c4d5421baa8fb5
Glenn Mohre <glenn.mohre@gmail.com>
2019-03-11 14:50:08 -0400

    added query object using as cached queries

--- heroes/api.py
+++ heroes/api.py
@@ -5,37 +5,29 @@ import json
 from flask import Blueprint, request, jsonify
 from flask import current_app as app
 from werkzeug.exceptions import NotFound
-from .models import Hero, db
-from .util import marvel_hash
+from .models import Hero, MarvelQuery, db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
+@bp.route('/heroes', methods=['GET'])
+def heroes():
+    heroes = Hero.query.all() or []
+    return jsonify([hero.serialize() for hero in heroes])
 
 @bp.route('/query', methods=('POST',))
 def query():
-    hero_name = request.form.get('hero_name')
+    d = json.dumps(request.json)
+    d = request.json
+    hero_name = d.get('hero_name')
     if not hero_name:
         error = "Hero name required"
         return jsonify({'error':error})
     try:
-        hero = Hero.query.\
-            filter(Hero.name.ilike(f"%{hero_name}%")).first_or_404()
+        qs = f"name={hero_name}"
+        prev_query = MarvelQuery.query.filter_by(querystring=qs, api='characters').first_or_404()
+        return jsonify(prev_query.serialize())
     except NotFound as e:
-        ts, m_hash, api_key = marvel_hash()
-        hero_name = urllib.parse.quote(hero_name)
-        query_url = f"{app.config.get('MARVEL_BASE_URL')}/v1/public/characters?ts={ts}&name={hero_name}&hash={m_hash}&apikey={api_key}"
-        print(query_url)    
-        with urllib.request.urlopen(query_url) as response:
-            res = response.read()
-        data = json.loads(res.decode('utf-8')).get('data', {})
-        results = data.get('results', [])
-        if any(results):   
-            hero_res = results[0]
-            thumbnail = '.'.join(hero_res['thumbnail'].values())
-            hero = Hero(name=hero_res['name'], thumbnail_url=thumbnail, marvel_id=hero_res['id'])
-            db.session.add(hero)
-            db.session.commit()
-        else:
-            error = "No Hero"
-            return jsonify({'error':error})
-    return jsonify(hero.serialize())
+        query_res = MarvelQuery(querystring=qs, api='characters')
+        db.session.add(query_res)
+        db.session.commit()
+        return jsonify(query_res.serialize())
--- heroes/models.py
+++ heroes/models.py
@@ -1,9 +1,60 @@
+from flask import current_app as app
+import json
+import urllib
 from heroes import db
+from werkzeug.exceptions import NotFound
+
+from .util import marvel_hash
 
 team_heroes = db.Table('team_heroes', db.metadata,
     db.Column('team_id', db.Integer, db.ForeignKey('teams.id'), primary_key=True),
     db.Column('hero_id', db.Integer, db.ForeignKey('heroes.id'), primary_key=True)
 )
+
+class MarvelQuery(db.Model):
+    __tablename__ = 'marvelqueries'
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    querystring = db.Column(db.String)
+    api = db.Column(db.String())
+
+    def _get_marvel_data(self, api, qs):
+        ts, m_hash, api_key = marvel_hash()
+        if not all((ts, m_hash, api_key)):
+            raise NotFound('Query Data Missing')
+        print(api)
+        print(qs)
+        query_value = urllib.parse.quote(qs)
+        query_url = f"{app.config.get('MARVEL_BASE_URL')}/v1/public/{api}?ts={ts}&{qs}&hash={m_hash}&apikey={api_key}"
+        print(query_url)
+        with urllib.request.urlopen(query_url) as response:
+            res = response.read()
+        data = json.loads(res.decode('utf-8')).get('data', {})
+        results = data.get('results', [])
+        if any(results):
+            hero_res = results[0]
+            print(len(results))
+            thumbnail = '.'.join(hero_res['thumbnail'].values())
+            return dict(name=hero_res['name'], thumbnail_url=thumbnail, marvel_id=hero_res['id'])
+        else:
+            raise NotFound('hero')
+
+
+    def __init__(self, querystring, api):
+        self.api = api
+        self.querystring = querystring
+        marvel_data = self._get_marvel_data(api, querystring)
+        hero = Hero(marvel_data['name'], marvel_data['thumbnail_url'], marvel_data['marvel_id'])
+        db.session.add(hero)
+        db.session.commit()
+        self.hero_id = hero.id
+    
+    def serialize(self):
+        return {
+            'querystring' : self.querystring,
+            'api' : self.api,
+            'hero' : self.hero.serialize()
+        }
 
 class Hero(db.Model):
     __tablename__ = 'heroes'
@@ -11,7 +62,9 @@ class Hero(db.Model):
     marvel_id = db.Column(db.Integer)
     name = db.Column(db.String)
     thumbnail_url = db.Column(db.String())
-    
+    queries = db.relationship('MarvelQuery', backref='hero', lazy=True)
+
+
     def __init__(self, name, thumbnail_url, marvel_id):
         self.name = name
         self.thumbnail_url = thumbnail_url
